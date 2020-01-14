79706d2d3da83f70d4ec3ef65d55835f9a76feeb
Andrii_Tarnovetskyi <youketeroamano22@gmail.com>
2020-01-14 17:48:12 +0200

    initial commit1

---
+++ src/com/epam/Main.java
@@ -0,0 +1,9 @@
+package com.epam;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sljfldsjflsdjflksj");
+        System.out.println(123);
+    }
+}
